6d301fa7e3f405965e3f7809374317c4a352260e
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-11-23 17:49:39 +0000

    primeeeeees everywhereeeere

--- exercises/eratosthene.cpp
+++ exercises/eratosthene.cpp
@@ -1,15 +1,20 @@
 #include "../lib.cpp"
 
+
+// using namespace chrono;
+// using cclock = chrono::steady_clock;
+
+
 // sieve & this function has equal runtime at small range (1000)
 // but in 1 000 000 sieve way faster
 
-// bool prime(int n){
-//     if(n==2)return true;
-//     for(int a=2;a<n/2;++a){
-//         if(n%a==0) return false;
-//     }
-//     return true;
-// };
+bool prime(int n){
+    if(n==2)return true;
+    for(int a=2;a<n/2;++a){
+        if(n%a==0) return false;
+    }
+    return true;
+};
 
 int main(){
     bool primes[1000000]={true,true}; // all 0 , false==prime
@@ -26,10 +31,16 @@ int main(){
     for(size_t i=2;i<sizeof(primes);++i)
         if(!primes[i])
             cout<<i<<endl;
-
-    // for(int i=2; i<1000000;++i)
+    
+    // time_t now=time(0);
+    // cout<<"begin__"<<now<<endl;
+    
+    // auto tic = cclock::now();
+    
+    // for(int i=2; i<10000;++i)
     //     if(prime(i))
     //         cout<<i<<endl;
-    
+    // auto toc = cclock::now();
+    // cout << "Elapsed time: " << duration_cast<milliseconds>(toc - tic).count() << endl;
 }
 
