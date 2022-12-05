81b7c497edbfc1f601e875554ec0b019acb09d50
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-05 13:30:09 +0000

    main with arguments

---
+++ exercises/cmd.cpp
@@ -0,0 +1,36 @@
+/*
+a)write a pprogram that outputs its own name and all command line arguments each in separate line
+b)now extend the prgram to output its own environment. the environment is a memomry area containing string in the format
+
+a third parameter for the function main() allows access to the environment this parameter is an array of pointers just like
+argv the array elements are char pointers to the environme tstring the last element being a null pointer*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[], char* argenv[])
+{
+    
+
+    cout<<" Program: "<<*argv<<endl;
+
+    cout<<"\nCommand line arguments :"<<endl;
+    int i;
+    for(i=1;i<argc;++i)
+        cout<<argv[i]<<endl;
+
+    cout<<"type <return> to go on";
+    cin.get();
+
+    cout<<"Environment strings :"<<endl;
+    // for(i=0;argenv[i]=NULL;++i)
+    //     cout<<argenv[i]<<endl;
+
+    // idk upper version dumpes core, ill use my own
+    for(size_t t=0;*argenv!=NULL;++t)
+    {
+        cout<<*argenv++<<endl;
+    }
+
+    return 0;
+}
